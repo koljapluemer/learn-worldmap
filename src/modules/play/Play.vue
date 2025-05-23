96c4dc69205a1f06d86772097649be0655d0b764
koljapluemer <github@koljapluemer.com>
2025-05-23 18:12:18 +0200

    factor Filter buttons into their own component

--- src/modules/play/Play.vue
+++ src/modules/play/Play.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
-import { ref, computed, watch } from 'vue'
+import { computed, watch } from 'vue'
 import { useGeographyLearning } from '@/modules/spaced-repetition-learning/calculate-learning/useGeographyLearning'
 import { useLearningProgress } from '@/modules/standard-play-progress-bar/useLearningProgress'
 import WorldMapGame from '@/modules/map-renderer/WorldMapGame.vue'
-import FilterModal from '@/modules/filter-modal/FilterModal.vue'
 import { useCountrySelection } from '@/modules/filter-modal/selections/useCountrySelection'
 
 import allCountries from '@/modules/map-data/country-lists/all-countries.json'
-import IconFilter from '../icons/IconFilter.vue'
-import IconSettings from '../icons/IconSettings.vue'
-import IconStats from '../icons/IconStats.vue'
+import Header from './header-and-modals/Header.vue'
 
 const { targetCountryToClick, handleGameCompletion, setAvailableCountries, selectRandomCountry } = useGeographyLearning()
 const { setAvailableCountries: setProgressCountries, updateProgress } = useLearningProgress()
@@ -19,9 +16,7 @@ const { setAvailableCountries: setProgressCountries, updateProgress } = useLearn
 const { selectedCountries } = useCountrySelection()
 
 // Modal state
-const isFilterModalOpen = ref(false)
-const isSettingsModalOpen = ref(false)
-const isStatsModalOpen = ref(false)
+
 // Filtered countries based on selected countries
 const filteredCountries = computed(() => {
   return allCountries.filter(country => selectedCountries.value.includes(country))
@@ -43,33 +38,7 @@ watch(filteredCountries, (newVal) => {
 </script>
 
 <template>
-  <header class="flex flex-row justify-between items-center p-1">
-    <h1 class="font-bold">Learn the World Map</h1>
-
-    <nav class="flex flex-row gap-2">
-      <button 
-        class="btn"
-        @click="isFilterModalOpen = true"
-      >
-        <IconFilter />
-        <span class="hidden md:block">Filter Countries</span>
-      </button>
-      <button 
-        class="btn"
-        @click="isSettingsModalOpen = true"
-      >
-        <IconSettings />
-        <span class="hidden md:block">Settings</span>
-      </button>
-      <button 
-        class="btn"
-        @click="isStatsModalOpen = true"
-      >
-        <IconStats />
-        <span class="hidden md:block">Stats</span>
-      </button>
-    </nav>
-  </header>
+  <Header />
   <main>
     <!-- Game Component -->
     <WorldMapGame
@@ -88,10 +57,6 @@ watch(filteredCountries, (newVal) => {
       </div>
     </div>
 
-    <!-- Filter Modal -->
-    <FilterModal
-      :is-open="isFilterModalOpen"
-      @close="isFilterModalOpen = false"
-    />
+
   </main>
 </template>
---
+++ src/modules/play/header-and-modals/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <header class="flex flex-row justify-between items-center p-1">
+    <h1 class="font-bold">Learn the World Map</h1>
+
+    <nav class="flex flex-row gap-2">
+      <button 
+        class="btn"
+        @click="isFilterModalOpen = true"
+      >
+        <IconFilter />
+        <span class="hidden md:block">Filter Countries</span>
+      </button>
+      <button 
+        class="btn"
+        @click="isSettingsModalOpen = true"
+      >
+        <IconSettings />
+        <span class="hidden md:block">Settings</span>
+      </button>
+      <button 
+        class="btn"
+        @click="isStatsModalOpen = true"
+      >
+        <IconStats />
+        <span class="hidden md:block">Stats</span>
+      </button>
+    </nav>
+  </header>
+
+  <FilterModal
+      :is-open="isFilterModalOpen"
+      @close="isFilterModalOpen = false"
+    />
+</template>
+
+
+<script setup lang="ts">
+
+import { ref } from 'vue'
+import IconFilter from '@/modules/icons/IconFilter.vue'
+import IconSettings from '@/modules/icons/IconSettings.vue'
+import IconStats from '@/modules/icons/IconStats.vue'
+import FilterModal from '@/modules/filter-modal/FilterModal.vue'
+
+const isFilterModalOpen = ref(false)
+const isSettingsModalOpen = ref(false)
+const isStatsModalOpen = ref(false)
+</script>
